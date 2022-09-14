4290c6b1fbcc4462dc9bb3086f29a572c24df304
Aziyev <majorik.99@inbox.ru>
2022-09-14 22:14:28 +0400

    v.04

--- WorldTelecomFinalProj/WorldTelecomFinalProj/Controllers/HomeController.cs
+++ WorldTelecomFinalProj/WorldTelecomFinalProj/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using WorldTelecomFinalProj.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WorldTelecomFinalProj.Models;
 
 
 
@@ -13,7 +13,6 @@ namespace WorldTelecomFinalProj.Controllers
 {
     public class HomeController : Controller
     {
-
         private readonly AppDbContext _context;
         public HomeController(AppDbContext context)
         {
---
+++ WorldTelecomFinalProj/WorldTelecomFinalProj/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorldTelecomFinalProj.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorldTelecomFinalProj.Models;
+
+namespace WorldTelecomFinalProj.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+        public async Task<IActionResult> Product(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+
+            }
+            Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+    }
+}
